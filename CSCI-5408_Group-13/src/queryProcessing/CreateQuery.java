f00e6f31665b36098778fc9f447d043a36e298d2
Deep_Prashantkumar_Patel <dp889845@dal.ca>
2021-04-07 09:05:42 -0300

    Create query implemented

--- CSCI-5408_Group-13/src/queryProcessing/CreateQuery.java
+++ CSCI-5408_Group-13/src/queryProcessing/CreateQuery.java
@@ -3,85 +3,54 @@ package queryProcessing;
 import java.util.*;
 
 public class CreateQuery {
-	public void createQueryOperations(String query) {
-
-		System.out.println("create");
+	public Map<String, Map<String, Map<String, List<String>>>> createQueryOperations(String query) {
 
 		// Ex: Map<"CollageManagement",Map<"student",Map<"id",List<["111",112""]>>>>
 		// Ex: Map<"CollageManagement",Map<"student",Map<"name",List<["deep","prabhnoor"]>>>>
 		// Ex: Map<"CollageManagement",Map<"student",Map<"department",List<["MACS","MACS"]>>>>
+		Map<String, Map<String, Map<String, List<String>>>> mapDatabaseData = new HashMap<>();
 
-
-		Map<String,Map<String,Map<String,List<String>>>> objDatabaseData = new HashMap<>();
+		// Ex: Map<"student",Map<"id",List<["id","varchar"]>>>
+		Map<String, Map<String, List<String>>> mapTableStructure = new HashMap<>();
 
 		// Map<"id", List<["int","pk"]>>
 		// Map<"name", List<"varchar">>
 		// Map<"department", List<"varchar">>
-		Map<String,List<String>> mapTableStructure = new HashMap<>();
+		Map<String, List<String>> mapColumn = new HashMap<>();
 
+		String currentTable = "";
 		String[] splitQueryForProcessing = query.split(" ");
-		splitQueryForProcessing[0].replace("create","");
+		splitQueryForProcessing[0].replace("create", "");
 
-		try{
+		try {
+			for (int i = 0; i < splitQueryForProcessing.length; i++) {
+				if (splitQueryForProcessing[i].equalsIgnoreCase("create")) {
+					currentTable = splitQueryForProcessing[i + 2];
+					mapTableStructure.put(splitQueryForProcessing[i + 2], null);
+					i += 2;
+				} else if (splitQueryForProcessing[i].equalsIgnoreCase("(") || splitQueryForProcessing[i].equalsIgnoreCase(",")) {
+					List<String> tempList = new ArrayList<>();
+					tempList.add(splitQueryForProcessing[i + 2]);
+					mapColumn.put(splitQueryForProcessing[i + 1], tempList);
 
-		}catch(IllegalArgumentException e){
-			System.out.println();
-		}
-
-//		CREATE TABLE Persons (
-//				PersonID int,
-//				LastName varchar(255),
-//				FirstName varchar(255),
-//				Address varchar(255),
-//				City varchar(255)
-//		);
-
-//		table : student
-//		tStart
-//		tStructStart
-//		col : id int | pk
-//		col : name varchar
-//		col : department varchar
-//		tStructEnd
-//		rowStart
-//		id : 111
-//		name : deep
-//		department : MACS
-//		rowEnd
-//		rowStart
-//		id : 112
-//		name : deep
-//		department : MACS
-//		rowEnd
-//		tEnd
-
-
-		String formattedStringToStore = "";
-		for(String str : splitQueryForProcessing){
-			if(str.equalsIgnoreCase("table")){
-
-				formattedStringToStore += "\ntable : " + splitQueryForProcessing[Arrays.asList(splitQueryForProcessing).indexOf(str) + 1];
-				formattedStringToStore += "\ntstart";
-				formattedStringToStore += "\ntStructStart";
-			}
-			else if(str.equalsIgnoreCase("(")){
-				List<String> tempList = new ArrayList<>();
-				tempList.add(splitQueryForProcessing[Arrays.asList(splitQueryForProcessing).indexOf(str) + 2]);
-				mapTableStructure.put(splitQueryForProcessing[Arrays.asList(splitQueryForProcessing).indexOf(str) + 1],tempList);
-
-				//formattedStringToStore += "\ncol : " + splitQueryForProcessing[Arrays.asList(splitQueryForProcessing).indexOf(str) + 1] + splitQueryForProcessing[Arrays.asList(splitQueryForProcessing).indexOf(str) + 2];
-			}
-			else if(str.equalsIgnoreCase(")")){
-				String pkConstratint = splitQueryForProcessing[Arrays.asList(splitQueryForProcessing).indexOf(str) + 1];
-				if(pkConstratint != null ){
-					if(pkConstratint.equalsIgnoreCase("constraint")){
-
+					if (splitQueryForProcessing[i + 3].equalsIgnoreCase("primary")) {
+						tempList.add("pk");
+						i += 3;
+					} else {
+						tempList.add(null);
+						i += 2;
 					}
+				} else if (splitQueryForProcessing[i].equalsIgnoreCase(")")) {
+					mapTableStructure.put(currentTable, mapColumn);
+				} else if (splitQueryForProcessing[i].equalsIgnoreCase(";")) {
+					mapDatabaseData.put("database", mapTableStructure);
 				}
 			}
-			else if(str.equalsIgnoreCase(",")){
-				formattedStringToStore += "\ncol : " + splitQueryForProcessing[Arrays.asList(splitQueryForProcessing).indexOf(str) + 1] + splitQueryForProcessing[Arrays.asList(splitQueryForProcessing).indexOf(str) + 2];
-			}
+		} catch (IllegalArgumentException e) {
+			System.out.println("Invalid Query");
 		}
+		return mapDatabaseData;
 	}
 }
+
+// create table student ( id int primary key , name varchar , department varchar ) ;
