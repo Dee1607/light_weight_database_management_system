f15df1bf32088f6fce16ce7b80abeccd331aa4f1
Deep_Prashantkumar_Patel <dp889845@dal.ca>
2021-04-07 18:46:55 -0300

    select query updated

--- CSCI-5408_Group-13/src/queryProcessing/SelectQuery.java
+++ CSCI-5408_Group-13/src/queryProcessing/SelectQuery.java
@@ -1,6 +1,5 @@
 package queryProcessing;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,20 +9,26 @@ public class SelectQuery {
 
 		Map<String, Map<String, List<String>>> tempTables = mapExistingData.get("CollageManagement");
 
-		String[] queryWords = query.split(" ");
+		query = query.replaceAll(";","");
+		query = query.replaceAll(",","");
+		query = query.replaceAll("\\(","");
+		query = query.replaceAll("\\)","");
 
-		Map<String,List<String>> tempColumn = tempTables.get(queryWords[3]);
+		String[] tableNameData = query.split(" ");
+		Map<String,List<String>> tempColumn = tempTables.get(tableNameData[3]);
 
-		for (String columnName : tempColumn.keySet()){
-			List<String> columnValues = new ArrayList<>();
-			columnValues = tempColumn.get(columnName);
-			System.out.print(columnName+":");
-			for (int i = 0; i <columnValues.size() ; i++) {
-				System.out.print(columnValues.get(i)+"  ");
-				System.out.println("");
+
+		System.out.println("=================================================================");
+		for(String columnName : tempColumn.keySet()){
+			System.out.format("%1s%15s%15s", "|",columnName + ":", " |");
+
+			List<String> tempListValues = tempColumn.get(columnName);
+			for (int i = 2; i < tempListValues.size(); i++) {
+				System.out.format("%1s%15s%15s", "|",tempListValues.get(i), " |");
 			}
+			System.out.println();
 		}
-
+		System.out.println("=================================================================");
 	}
 }
 
