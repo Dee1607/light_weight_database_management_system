bfb6bfd39cc97b4b2b5ba99f28823991876a0943
Deep_Prashantkumar_Patel <dp889845@dal.ca>
2021-04-07 09:06:15 -0300

    Delete query implemented

--- CSCI-5408_Group-13/src/queryProcessing/DeleteQuery.java
+++ CSCI-5408_Group-13/src/queryProcessing/DeleteQuery.java
@@ -1,8 +1,41 @@
 package queryProcessing;
 
+import java.util.List;
+import java.util.Map;
+
 public class DeleteQuery {
-	public void deleteQueryOperations(String query) {
+	public Map<String,Map<String, Map<String, List<String>>>> deleteQueryOperations(String query, Map<String,Map<String, Map<String, List<String>>>> mapExistingData) {
 		
 		System.out.println("delete");
+		Map<String, Map<String, List<String>>> tempTables = mapExistingData.get("CollageManagement");
+
+		query = query.replaceAll(";","");
+		query = query.replaceAll(",","");
+		query = query.replaceAll("\\(","");
+		query = query.replaceAll("\\)","");
+
+		String[] tableNameData = query.split(" ");
+		Map<String,List<String>> tempColumn = tempTables.get(tableNameData[2]);
+
+		String[] split = query.split("where");
+		String[] rightSplit = split[1].split("=");
+
+		int index = -1;
+		List<String> tempList = tempColumn.get(rightSplit[0].trim());
+		index = tempList.indexOf(rightSplit[1].trim());
+
+		if(index >= 0){
+			for(String str : tempColumn.keySet()){
+				List<String> temparoryData = tempColumn.get(str);
+				temparoryData.remove(index);
+				tempColumn.replace(str,temparoryData);
+			}
+			tempTables.replace(tableNameData[2].trim(),tempColumn);
+			mapExistingData.replace("CollageManagement", tempTables);
+		}
+
+		return mapExistingData;
 	}
 }
+
+//delete from student where id = 111 ;
