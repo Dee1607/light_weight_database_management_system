377b3e6d676628acc4b6eba02758d462a7326e76
Deep_Prashantkumar_Patel <dp889845@dal.ca>
2021-04-07 09:03:06 -0300

    Login Page Authenticated

--- CSCI-5408_Group-13/src/login/LoginPage.java
+++ CSCI-5408_Group-13/src/login/LoginPage.java
@@ -1,27 +1,63 @@
 package login;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import presentationlayer.DisplayToGetUserChoice;
 
 public class LoginPage {
 
-	DisplayToGetUserChoice objGetData = null;
+	private String USER_CREDENTIALS_FILE_PATH = "/Users/deeppatel/Desktop/GlobalDataDictionary/users.txt";
+	private DisplayToGetUserChoice objGetData = null;
+	Map<String,String> MAP_OF_CREDENTIALS = null;
+
 	public boolean getCredentials() 
 	{
-		objGetData = new DisplayToGetUserChoice();
-		String username = objGetData.displayMessageGetStringChoiceFromUser("Enter Username: ");
-		String password = objGetData.displayMessageGetStringChoiceFromUser("Enter Password: ");
-		
-		boolean isCredentialsCorrect = checkUser(username,password);
-		
+		MAP_OF_CREDENTIALS = new HashMap<>();
+		boolean isCredentialsCorrect = false;
+
+		try{
+			File file = new File(USER_CREDENTIALS_FILE_PATH);
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			while((line = br.readLine()) != null){
+				String[] tempListOfUsers = line.split(":");
+				MAP_OF_CREDENTIALS.put(tempListOfUsers[0],tempListOfUsers[1]);
+			}
+
+			objGetData = new DisplayToGetUserChoice();
+
+			int count = 0;
+			while(!isCredentialsCorrect){
+				if(count != 0){
+					objGetData.displayMessage("Invalid Password!! Please Enter Again!");
+				}
+				String username = objGetData.displayMessageGetStringChoiceFromUser("Enter Username: ");
+				String password = objGetData.displayMessageGetStringChoiceFromUser("Enter Password: ");
+				isCredentialsCorrect = checkUser(username,password,MAP_OF_CREDENTIALS);
+				if(count >= 3){
+					objGetData.displayMessage("You are Blocked!!");
+					return false;
+				}
+				count++;
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		return isCredentialsCorrect;
 	}
-	public boolean checkUser(String username, String password) {
-		
-		//Check from remote file/local file.
-		
-		return true;
+	public boolean checkUser(String username, String password,Map<String,String> mapCheckingCredentials) {
+
+		if(mapCheckingCredentials.containsKey(username)){
+			if(mapCheckingCredentials.get(username).equals(password)){
+				return true;
+			}
+		}
+		return false;
 	}
-	
 }
