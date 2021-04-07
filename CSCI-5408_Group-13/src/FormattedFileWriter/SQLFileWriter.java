5e37a45dfa4a6c5db6ba944ce59698656cfea9e6
Deep_Prashantkumar_Patel <dp889845@dal.ca>
2021-04-07 12:51:54 -0300

    File Writer Implemented

--- CSCI-5408_Group-13/src/FormattedFileWriter/SQLFileWriter.java
+++ CSCI-5408_Group-13/src/FormattedFileWriter/SQLFileWriter.java
@@ -1,46 +1,76 @@
 package FormattedFileWriter;
 
-import java.util.List;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
 
 public class SQLFileWriter {
 
-    public void writeIntoSQLFile(Map<String,Map<String, Map<String, List<String>>>> mapDatabaseData){
+    public void writeIntoSQLFile(Map<String, Map<String, Map<String, List<String>>>> mapDatabaseData) {
         String formattedString = "";
 
-        for(String databaseName : mapDatabaseData.keySet()){
+        Map<String, List<String>> VALUES_OF_TABLE = new HashMap<>();
+
+
+        Map<String, Map<String, Map<String, List<String>>>> tempDatabaseData = new HashMap<>();
+        Map<String, Map<String, List<String>>> tempTableData = new HashMap<>();
+        Map<String, List<String>> tempColumnData = new HashMap<>();
+        List<String> values = new ArrayList<>();
+
+        for (String databaseName : mapDatabaseData.keySet()) {
 
             formattedString += "database : " + databaseName;
             formattedString += "\n" + "dbStart";
 
-            for(String tableName : mapDatabaseData.get(databaseName).keySet()){
-                formattedString += "table : " + tableName;
+            for (String tableName : mapDatabaseData.get(databaseName).keySet()) {
+                formattedString += "\ntable : " + tableName;
                 formattedString += "\n" + "tStart";
                 formattedString += "\n" + "tStructStart";
 
-                for(String columnName : mapDatabaseData.get(databaseName).get(tableName).keySet()){
+                Set<String> tempSet = mapDatabaseData.get(databaseName).get(tableName).keySet();
 
-                    formattedString += "\n"+"col : " + columnName + " " + mapDatabaseData.get(databaseName).get(tableName).get(columnName).get(0);
+                for (String columnName : mapDatabaseData.get(databaseName).get(tableName).keySet()) {
+                    formattedString += "\n" + "col : " + columnName + " " + mapDatabaseData.get(databaseName).get(tableName).get(columnName).get(0);
 
-                    if(mapDatabaseData.get(databaseName).get(tableName).get(columnName).get(1) != null){
+                    if (mapDatabaseData.get(databaseName).get(tableName).get(columnName).get(1) != null) {
                         formattedString += " | " + "pk";
-                    }
-                    if(mapDatabaseData.get(databaseName).get(tableName).get(columnName).get(2) != null){
-                        formattedString += "\nrowStart";
-                        for(String column : mapDatabaseData.get(databaseName).get(tableName).keySet()){
-
-                        }
-
                     }
                 }
                 formattedString += "\n" + "tStructEnd";
-
-
-
+                for (String columnName : mapDatabaseData.get(databaseName).get(tableName).keySet()) {
+                    for (int i = 2; i < mapDatabaseData.get(databaseName).get(tableName).get(columnName).size(); i++) {
+                        formattedString += "\n" + "rowStart";
+                        for (String str : tempSet) {
+                            formattedString += "\n" + str + " : " + mapDatabaseData.get(databaseName).get(tableName).get(str).get(i) + " | ";
+                        }
+                        formattedString += "\n" + "rowEnd";
+                    }
+                    break;
+                }
                 formattedString += "\n" + "tEnd";
             }
-            formattedString += "\n" + "dbEnd";
+            formattedString += "\n" + "dbEnd" + "\n";
         }
-        System.out.println(formattedString);
+        try{
+            Path path = Path.of("databaseData.txt");
+            Files.createFile(path);
+            Files.write(path,formattedString.toString().getBytes(StandardCharsets.UTF_8), StandardOpenOption.TRUNCATE_EXISTING);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void createDumpFiles(List<String> listOfDumps){
+        try{
+            Path path = Path.of("sqlDumpStored.txt");
+            Files.createFile(path);
+            for(String query : listOfDumps){
+                Files.write(path,query.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
